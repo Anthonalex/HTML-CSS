33571ac46d74e9c0971bb5d1868cefc8e44b652b
Anthonalex <Anthonalex@users.noreply.github.com>
2021-03-13 22:16:37 +0400

    finall commit, page done works great!

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ menuBtnBurger.addEventListener("click", function () {
     menu.src = "./imgs/menu.png"
   }
 });
+
+// All animations will take exactly 500ms
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	speedAsDuration: true
+});
